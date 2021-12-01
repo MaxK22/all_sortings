85bbebb146709f481111a4d1506b8c0f122e867e
MaxK <korotkovmaxim.a@gmail.com>
2021-12-01 10:51:39 +0300

    bubble

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ vector<int> sort90_massive_size_5;
 vector<int> sort90_massive_size_7;
 
 
-void bubble_sort(vector<int> m)
+void bubble_sort_exit(vector<int> m)
 {
     bool f = true;
     for(int i = 1; i < m.size() && f; i++)
@@ -35,7 +35,101 @@ void bubble_sort(vector<int> m)
     }
 }
 
+void bubble_sort(vector<int> m)
+{
+    for(int i = 1; i < m.size(); i++)
+    {
+        for(int g = 0; g < m.size() - i; g++)
+        {
+            if(m[g] > m[g+1])
+            {
+                swap(m[g], m[g+1]);
+            }
+        }
+    }
+}
 
+void test(void (*sort_function)(vector<int>), int dif, string name)
+{
+    cout << endl << endl;
+    cout << "   " + name + ": " << endl;
+    double ticksStart, timeUsed;
+    if(dif == 1) {
+        ticksStart = clock();
+        sort_function(sort_massive_size_3);
+        timeUsed = (clock() - ticksStart) / (double) CLOCKS_PER_SEC;
+        cout << "sort sort mass with size 10^3: " << timeUsed << endl;
+        ticksStart = clock();
+        sort_function(sort_massive_size_5);
+        timeUsed = (clock() - ticksStart) / (double) CLOCKS_PER_SEC;
+        cout << "sort sort mass with size 10^5: " << timeUsed << endl;
+
+        ticksStart = clock();
+        sort_function(rand_massive_size_3);
+        timeUsed = (clock() - ticksStart) / (double) CLOCKS_PER_SEC;
+        cout << "sort rand mass with size 10^3: " << timeUsed << endl;
+        ticksStart = clock();
+        sort_function(rand_massive_size_5);
+        timeUsed = (clock() - ticksStart) / (double) CLOCKS_PER_SEC;
+        cout << "sort rand mass with size 10^5: " << timeUsed << endl;
+
+        ticksStart = clock();
+        sort_function(resort_massive_size_3);
+        timeUsed = (clock() - ticksStart) / (double) CLOCKS_PER_SEC;
+        cout << "sort reverse mass with size 10^3: " << timeUsed << endl;
+        ticksStart = clock();
+        sort_function(resort_massive_size_5);
+        timeUsed = (clock() - ticksStart) / (double) CLOCKS_PER_SEC;
+        cout << "sort reverse mass with size 10^5: " << timeUsed << endl;
+
+        ticksStart = clock();
+        sort_function(sort90_massive_size_3);
+        timeUsed = (clock() - ticksStart) / (double) CLOCKS_PER_SEC;
+        cout << "sort 90 percent sort mass with size 10^3: " << timeUsed << endl;
+        ticksStart = clock();
+        sort_function(sort90_massive_size_5);
+        timeUsed = (clock() - ticksStart) / (double) CLOCKS_PER_SEC;
+        cout << "sort 90 percent sort mass with size 10^5: " << timeUsed << endl;
+    }
+    else
+    {
+        ticksStart = clock();
+        sort_function(sort_massive_size_5);
+        timeUsed = (clock() - ticksStart) / (double) CLOCKS_PER_SEC;
+        cout << "sort sort mass with size 10^5: " << timeUsed << endl;
+        ticksStart = clock();
+        sort_function(sort_massive_size_7);
+        timeUsed = (clock() - ticksStart) / (double) CLOCKS_PER_SEC;
+        cout << "sort sort mass with size 10^7: " << timeUsed << endl;
+
+        ticksStart = clock();
+        sort_function(rand_massive_size_5);
+        timeUsed = (clock() - ticksStart) / (double) CLOCKS_PER_SEC;
+        cout << "sort rand mass with size 10^5: " << timeUsed << endl;
+        ticksStart = clock();
+        sort_function(rand_massive_size_7);
+        timeUsed = (clock() - ticksStart) / (double) CLOCKS_PER_SEC;
+        cout << "sort rand mass with size 10^7: " << timeUsed << endl;
+
+        ticksStart = clock();
+        sort_function(resort_massive_size_5);
+        timeUsed = (clock() - ticksStart) / (double) CLOCKS_PER_SEC;
+        cout << "sort reverse mass with size 10^5: " << timeUsed << endl;
+        ticksStart = clock();
+        sort_function(resort_massive_size_7);
+        timeUsed = (clock() - ticksStart) / (double) CLOCKS_PER_SEC;
+        cout << "sort reverse mass with size 10^7: " << timeUsed << endl;
+
+        ticksStart = clock();
+        sort_function(sort90_massive_size_5);
+        timeUsed = (clock() - ticksStart) / (double) CLOCKS_PER_SEC;
+        cout << "sort 90 percent sort mass with size 10^5: " << timeUsed << endl;
+        ticksStart = clock();
+        sort_function(sort90_massive_size_7);
+        timeUsed = (clock() - ticksStart) / (double) CLOCKS_PER_SEC;
+        cout << "sort 90 percent sort mass with size 10^7: " << timeUsed << endl;
+    }
+}
 //
 
 int main() {
@@ -68,17 +162,8 @@ int main() {
 
 
     //
-    cout << endl << endl;
-    cout << "   Bubble sort: " << endl;
-    double ticksStart = clock();
-    bubble_sort(sort_massive_size_3);
-    double timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
-    cout << "sort sorting mass with size 10^3: " << timeUsed << endl;
-    ticksStart = clock();
-    bubble_sort(sort_massive_size_5);
-    timeUsed = (clock() - ticksStart) / (double)CLOCKS_PER_SEC;
-    cout << "sort sorting mass with size 10^5: " << timeUsed << endl;
-
+    test(bubble_sort, 1, "Bubble sort");
+    test(bubble_sort_exit, 1, "Bubble sort with exit");
 
 
     return 0;
